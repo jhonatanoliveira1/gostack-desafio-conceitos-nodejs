c5fb1b13e29f7c4709bcf99c25001c6f2a046855
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2020-06-22 17:44:57 -0300

    PUT /repositories/:id

--- src/app.js
+++ src/app.js
@@ -33,7 +33,25 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.'});
+  };
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repositories);
 });
 
 app.delete("/repositories/:id", (request, response) => {
